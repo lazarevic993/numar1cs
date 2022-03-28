28834d18d79fee21b032ec1c3e3b40bf87d39702
Stefan Lazarević <>
2022-03-28 12:34:07 +0200

    Init player service

---
+++ playerService/src/main/java/core/playerService/PlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package core.playerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerServiceApplication.class, args);
+	}
+
+}
