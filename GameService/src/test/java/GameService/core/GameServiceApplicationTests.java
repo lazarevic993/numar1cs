3992a6f212a28055a26ab06f03b97d84933bb8bf
Stefan Lazarević <>
2022-03-29 20:12:16 +0200

    Init commit for GameService

---
+++ GameService/src/test/java/GameService/core/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package GameService.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
